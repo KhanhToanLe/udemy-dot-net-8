6b7671c99296c83c37342ac28d3c245160525f67
ToanLK <khanhtoan.le.6555@gmail.com>
2024-03-14 08:55:39 +0700

    first commit

---
+++ WebApp/util/JsonHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.Json;
+
+namespace WebApp.util
+{
+    public static class JsonHelper
+    {
+        public static void LogObject(this Object logObject)
+        {
+            var logString =  JsonSerializer.Serialize(logObject);
+            Console.WriteLine(logString);
+        }
+    }
+}
